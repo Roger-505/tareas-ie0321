ae48e3f3e41c0737ff910647f2e3e0dcc34b0111
rogerdanielpiovetgarcia@gmail.com <rogerdanielpiovetgarcia@gmail.com>
2024-02-11 09:42:37 -0600

    feat: Se agregó la función solicitudMCD, y se agregaron encabezados para dos secciones del código

--- t3/C15990_tarea3_grupo901.s
+++ t3/C15990_tarea3_grupo901.s
@@ -26,20 +26,26 @@ main:
 	la $a0, msgSolicitud_a 	# inicializar $a0 = Dir[msgSolicitud_a]
 	la $a1, msgSolicitud_b	# incializar $a1 = Dir[msgSolicitud_b]
 	la $a2, msgError	# inicializar $a2 = Dir[msgError]
-	#jal solicitudInt	# saltar a la función solicitudInt para obtener a y b del usuario
+	jal solicitudInt	# saltar a la función solicitudInt para obtener a y b del usuario
 	
 	add $a0, $v0, 0 	# inicializar $a0 = a para la función mcd
 	add $a1, $v1, 0		# inicializar $a1 = b para la función mcd
-	#jal mcd			# saltar a la función mcd para calcular mcd(a,b)
+	jal mcd			# saltar a la función mcd para calcular mcd(a,b)
 
 	add $a0, $v0, 0		# inicializar $a0 = mcd(a,b) para imprimir mcd(a,b) 
 	la $a1, msgMCD    	# inicializar $a1 = Dir[msgMCD]
-	#jal print		# saltar a la función print para imprimir mcd(a,b)
+	jal print		# saltar a la función print para imprimir mcd(a,b)
 	
 	j main			# volver a solicitar a y b para calcular mcd(a,b)
 	
 solicitudInt:
-
+	la $a0, msgSolicitud_a	# inicializar $a0 = Dir[msgSolicitud_a]
+	addi $v0, $0, 4		# cargar código (4) para imprimir un string en la terminal
+	syscall
+	
+	addi $v0, $0, 5		# cargar código (5) para leer un int de la terminal
+	syscall
+	add $t0, $0, $v0	# $t0 = a
 mcd:
 
 print:
