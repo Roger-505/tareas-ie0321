779110c827970e7b480a3a7aa557d7b7708d62d7
rogerdanielpiovetgarcia@gmail.com <rogerdanielpiovetgarcia@gmail.com>
2024-02-10 18:20:42 -0600

    feat: Se agregó el encabezado al archivo

---
+++ t3/C15990_tarea3_grupo901.s
@@ -0,0 +1,16 @@
+#######################################
+#		Tarea 3		       #
+#	Estructuras de Computadores    #
+#		Digitales I	       #
+#	      Ciclo III 2023	       #
+#		Grupo 901	       #
+#	Roger Daniel Piovet García     #
+#		C15990		       #
+########################################
+
+# El siguiente repositorio de GitHub posee el código de esta 
+# tarea, al igual que el resto de las tareas del curso:
+#
+# https://github.com/Roger-505/tareas-ie0321.git
+
+
