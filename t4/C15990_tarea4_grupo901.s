ae349f5b92ef05820a8fcfd04cb70dc71d5c9c55
rogerdanielpiovetgarcia@gmail.com <rogerdanielpiovetgarcia@gmail.com>
2024-02-13 20:46:27 -0600

    feat: Se agregó la función printStr y readInt, implementadas en la tarea 3

--- t4/C15990_tarea4_grupo901.s
+++ t4/C15990_tarea4_grupo901.s
@@ -16,21 +16,32 @@
 # seccion de data
 .data
 msgFarey:	.asciiz "Ingrese un número natural para entregar la sucesión de Farey:\n"
+Fn:		.asciiz "SUCESION FAREY POR IMPLEMENTAR\n"
 
 # sección de texto
 .text
 main:
-	jal solicitudFarey		# saltar a la subrutina solicitudFarey, para obtener n del usuario 
+	la $a0, msgFarey 		# inicializar $a0 = Dir[msgFarey]
+	jal printStr			# saltar a la función printStr para imprimir msgFarey
+	jal readInt			# saltar a la función readInt para leer un int de la terminal, y devolver $v0 = n
 	
+	add $a0, $v0, $0		# cargar en $a0 = n digitado por el usuario
 	jal farey			# saltar a la subrutina farey, para calcular la F_n
 	
+	la $a0, Fn			# carga en $a0 = F_n
 	jal printStr			# saltar a la subrutina printStr, para imprimir F_n
 	
 	j main				# volver a solicitar n para calcular F_n
-
-solicitudFarey:
-	jr $ra
+	
+printStr:
+	addi $v0, $0, 4			# cargar código (4) en $v0 para imprimir string en $a0 = Dir[string]
+	syscall				# syscall para imprimir string
+	jr $ra				# volver al punto de llamado
+readInt:
+	addi $v0, $0, 5			# cargar código (5) en $v0 para leer un int de la terminal y guardar en $v0 = integer
+	syscall				# syscall para leer un int
+	jr $ra				# volver al punto de llamado
+	
 farey:
 	jr $ra
-printStr:
-	jr $ra
+
