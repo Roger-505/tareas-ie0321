ecc946314d5f28bb53955d109ed77ef824e023e4
rogerdanielpiovetgarcia@gmail.com <rogerdanielpiovetgarcia@gmail.com>
2024-02-13 20:34:51 -0600

    feat: Se agregó el programa principal con la etiqueta main: y el mensaje a imprimir en la sección .data

--- t4/C15990_tarea4_grupo901.s
+++ t4/C15990_tarea4_grupo901.s
@@ -12,3 +12,25 @@
 # tarea, al igual que el resto de las tareas del curso:
 #
 # https://github.com/Roger-505/tareas-ie0321.git
+
+# seccion de data
+.data
+msgFarey:	.asciiz "Ingrese un número natural para entregar la sucesión de Farey:\n"
+
+# sección de texto
+.text
+main:
+	jal solicitudFarey		# saltar a la subrutina solicitudFarey, para obtener n del usuario 
+	
+	jal farey			# saltar a la subrutina farey, para calcular la F_n
+	
+	jal printStr			# saltar a la subrutina printStr, para imprimir F_n
+	
+	j main				# volver a solicitar n para calcular F_n
+
+solicitudFarey:
+	jr $ra
+farey:
+	jr $ra
+printStr:
+	jr $ra
